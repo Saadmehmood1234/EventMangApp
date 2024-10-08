e7d33de429c6502a2fa1b946fe4d84cf790294f9
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-10-09 01:26:59 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ const Home = async() => {
         <p className='text-2xl text-gray-300'>I just Removed the Login and SignUp </p>
         <p className='text-green-600 text-lg '>I will add the Authentication and Authorization soon</p>
         <div>
+          
            <Link href={'/user'}>
            <button className='p-2 text-gray-700 text-xl bg-gray-400 rounded-md hover:bg-white border-none outline-none'>Go to the Event Page</button>
            </Link>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-export { auth as middleware } from "@/auth";
-
-export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
-
-// import { getToken } from 'next-auth/jwt';
-// import { NextResponse } from 'next/server';
-
-// export async function middleware(req:any) {
-//   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-// console.log(token)
-//   const protectedRoutes = ['/user', '/admin']; // Add any other protected routes
-
-//   // If the route is protected and there is no token, redirect to sign-in
-//   if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && !token) {
-//     return NextResponse.redirect(new URL('/signin', req.url));
-//   }
-// }
+// export { auth as middleware } from "@/auth";
 
 // export const config = {
-//   matcher: ['/user/:path*', '/admin/:path*'], // Protect routes under /user and /admin
+//     matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 // };
+// /middleware.js
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req:any) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+console.log(token)
+  const protectedRoutes = ['/user', '/admin']; // Add any other protected routes
+
+  // If the route is protected and there is no token, redirect to sign-in
+  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && !token) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+}
+
+export const config = {
+  matcher: ['/user/:path*', '/admin/:path*'], // Protect routes under /user and /admin
+};
 
 
 // import { getServerSession } from "next-auth/next";
