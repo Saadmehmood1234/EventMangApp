4bbdcca690abdf39e06bfe62d0eeeaa7cb01452c
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-10-09 01:35:42 +0530

    added

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,8 +10,8 @@ import { NextResponse } from 'next/server';
 export async function middleware(req:any) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 console.log(token)
-  const protectedRoutes = [ '/admin']; // Add any other protected routes
-
+//   const protectedRoutes = ['/user', '/admin']; // Add any other protected routes
+  const protectedRoutes = ['/'];
   // If the route is protected and there is no token, redirect to sign-in
   if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) && !token) {
     return NextResponse.redirect(new URL('/signin', req.url));
